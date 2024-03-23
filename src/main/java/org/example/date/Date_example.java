083a43e6d5523bff8c78a9a0ce0d70564d1c7c68
subbu <subbu97@gmail.com>
2024-03-23 20:17:37 +0530

    Adding learning project to git

---
+++ src/main/java/org/example/date/Date_example.java
@@ -0,0 +1,27 @@
+package org.example.date;
+
+import java.util.*;
+
+public class Date_example {
+
+    public static void main(String[] args)
+    {
+        int year = 2016;
+        int month = 0; // January
+        int date = 1;
+
+        Calendar cal = Calendar.getInstance();
+
+        // Sets the given calendar field value and the time value
+        // (millisecond offset from the Epoch) of this Calendar undefined.
+        cal.clear();
+        System.out.println();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DATE, date);
+
+        System.out.println(cal.getTime());
+        System.out.println();
+    }
+
+}
